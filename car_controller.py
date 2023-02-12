58a8fd867e05b91576b3d5b7c45bcc4aec80ab25
unknown <jimperson100@gmail.com>
2023-02-11 18:49:56 -0800

    before monkeying around with classes

--- car_controller.py
+++ car_controller.py
@@ -2,6 +2,7 @@ from email.policy import default
 import threading
 from time import sleep
 import pygame
+import motor_functions as mot
 
 # Switch input to controller
 print("Swap out keyboard for gamepad in 5 sec")
@@ -15,7 +16,13 @@ sleep(1.00)
 print("1 sec")
 sleep(1.00)
 
+# Set up motors
+print("Initializing motors")
+mot.setup_motors()
+sleep(3)
+
 # Gamepad setup
+print("Initializing gamepad")
 pygame.init()
 pygame.joystick.init()
 print("Num joysticks connected:")
@@ -70,14 +77,27 @@ def check_on_off():
         sleep(1/CHECK_ON_OFF_HZ)
 
 def send_motor_cmd():
-    #print("SEND MOTOR COMMAND LEFT = " + str(control_input['left_stick']))
-    #print("SEND MOTOR COMMAND RIGHT = " + str(control_input['right_stick']))
+    global control_input
+##    print("SEND MOTOR COMMAND LEFT = " + str(control_input['left_stick']))
+##    print("SEND MOTOR COMMAND RIGHT = " + str(control_input['right_stick']))
     while(True):
-        print("dummy motor command")
+##        print("dummy motor command")
+##        print("Commands R/L: " + str(control_input['right_stick']) + " " + str(conrol_input['left_stick']))
+##        print("SEND MOTOR COMMAND LEFT = " + str(control_input['left_stick']))
+##        print("SEND MOTOR COMMAND RIGHT = " + str(control_input['right_stick']))
+        if(control_input['right_stick'] < 0): #Right stick is right motor board is board 1
+            mot.direct_board1(-1)
+        elif(control_input['right_stick'] == 0):
+            mot.direct_board1(0)
+        else:
+            mot.direct_board1(1)
+        mot.set_speed_board1(100*abs(control_input['right_stick']))
+                           
+        
         sleep(1/MOTOR_CMD_HZ)
 
 t_read = threading.Thread(group=None, target=read_control_input)
-t_check = threading.Thread(group = None, target = check_on_off)
+##t_check = threading.Thread(group = None, target = check_on_off)
 t_cmd = threading.Thread(group = None, target = send_motor_cmd)
 
 t_read.start()
